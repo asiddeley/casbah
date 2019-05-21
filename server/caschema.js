61a21cd739b58cbec1d6832975a89d7c8c780e8e
Andrew Siddeley <andrew.siddeley@norr.com>
2019-05-21 14:37:00 -0400

    project schema

--- server/caschema.js
+++ server/caschema.js
@@ -9,57 +9,16 @@ const { gql } = require('apollo-server-express')
 
 var site=path.join(global.appRoot, global.casite)
 
-//gql is a tag literal function that ...
-exports.typeDefs=[ gql`
+//Combine typeDefs
+exports.typeDefs=[ 
+	//gql is a tag literal function that ...
+	gql`type Query { hello:String } schema {query:Query}`,
+	require("./project").typeDef
+]
 
-type Query {
-	hello:String
-	pronum:[String]
-	projectByNum(pronum:String!):Project
-}
-
-type Project {
-	pronum:String
-	name:String
-	address:String
-	owner:String
-	contractor:String
-	permit:String
-}
-
-schema {
-	query:Query
-}
-
-
-`]
-
-exports.resolvers={
-	//Note below how it's posible to define an object of functions without keys.
-	//https://www.apollographql.com/docs/tutorial/resolvers
-	Query:{
-		hello(){console.log("hello resolver..."); return 'world'},
-	
-		pronum(){
-			console.log("pronum resolver...")
-			//project numbers are the folder names in the site path 
-			return fs.readdirSync(site).filter(function (file) {
-				return fs.statSync(path.join(site,file)).isDirectory()
-			})
-		},
-		
-		projectByNum(parent, args, context, info){
-			
-			console.log("project resolver...", args)
-			//read datafile within each folder
-			var data
-			var file=path.join(site, args.pronum, "__data.json")
-			try {
-				data=fs.readFileSync(file)
-			} catch(e) {
-				data="{'error':'datafile not found'}"
-			}
-			return JSON.parse(data)
-		}				
-	}	
-}
+//Combine resolvers
+exports.resolvers={}
+exports.resolvers.Query=Object.assign(
+	{ hello(){console.log("hello resolver..."); return 'world'} },
+	require("./project").resolvers.Query
+)
