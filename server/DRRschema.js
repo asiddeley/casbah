89b25c845609dcc0934089d10b99e51a13d9b0fd
Andrew Siddeley <andrew.siddeley@norr.com>
2019-06-03 16:30:16 -0400

    DRRschema work ongoing

--- server/DRRschema.js
+++ server/DRRschema.js
@@ -16,7 +16,6 @@ const BLUE="\x1b[34m", RESET="\x1b[0m"
 
 
 
-
 ///////////////////////////////////////////////
 //export graphql Schema Definition Language (SDL)
 
@@ -55,50 +54,83 @@ type DrrHead {
 	reviewDate:String
 	reviewBy:String
 	reportDate:String	
+}`
+
+function DrrHead({projectId, drrId}){
+	this.drrId=drrId
+	this.reviewDate=new Date().toJSON().slice(0,10).replace(/-/g,'/')
+	this.reviewBy="ASiddeley"
+	this.reportDate="TBD"
 }
+const DrrHeadFile="__drrHead.json"
 
-
+exports.typeDefs+=`
 input DrrHeadInput {
 	reviewDate:String
 	reviewBy:String
-	reportDate:String	
-}
+	reportDate:String
+}`
 
+//no data funtion needed for Inputs - data supplied
+
+exports.typeDefs+=`
 type DrrGeneral {
-	id:String
+	num:Int
 	note:String
+	ref:String
 }
+`
 
+function DrrGeneral({projectId, drrId}){
+	this.id=1
+	this.comment="--"
+	this.ref="--"
+}
+const DrrGeneralFile="__drrGeneral.json"
+
+exports.typeDefs+=`
 type Deficiency {
 	id:String
 	description:String
 	deficiencyStatus:DeficiencyStatus
 }
+`
 
+function Deficiency({projectId, drrId}){
+	this.id=1
+	this.description="--"
+	this.deficiencyStatus="OPEN"
+}
+const DeficiencyFile="__deficiency.json"
+
+exports.typeDefs+=`
 type DrrByRoom{
 	room:String
 	checklist:String
 	comments:String
 	img:String
 }
-
+`
+exports.typeDefs+=`
 type DeficiencyFig{
 	id:String
 	img:String
 	title:String
 	date:String
 }
+`
 
+exports.typeDefs+=`
 enum DeficiencyStatus{
+	OPEN
 	CLOSED
 	CRITICAL
 	INFO
-	OPEN
 	NA
 }
 `
-///////////////////////////////////
-// Default values
+
+
 
 
 ////////////////////////////////
@@ -113,7 +145,7 @@ exports.resolvers={
 		var ids=[]
 
 		try {
-			var p=path.join(drr.prop.casite, projectId, drr.prop.branch)
+			var p=path.join(casite, projectId, branch)
 			ids=fs.readdirSync(p).filter(function (file) {
 				return fs.statSync(path.join(p,file)).isDirectory()
 			})			
@@ -125,14 +157,15 @@ exports.resolvers={
 	
 	drrHead({projectId, drrId}){
 		console.log("drrHead resolver...")
-		data=drr.data.drrHead //default data 
+		//data=drr.data.drrHead //default data 
+		data=new DrrHead()
 		try {
 			var p=path.join(
-				drr.prop.casite, 
+				casite, 
 				projectId, 
-				drr.prop.branch, 
+				branch, 
 				drrId, 
-				drr.prop.drrHeadFile
+				DrrHeadfile
 			)
 			data=JSON.parse(fs.readFileSync(p))
 		} catch(e) {console.log("error:",e)}
@@ -141,30 +174,31 @@ exports.resolvers={
 	
 	drrGenerals({projectId, drrId}){
 		console.log("drrGenerals resolver...")		
-		var data=drr.data.drrGenerals //default data	
+		var data=[new DrrGeneral()] //default data	
 		try {
-			var p=path.join(site, projectId, branch, drrId, drrGeneralsFile)
+			var p=path.join(casite, projectId, branch, drrId, DrrGeneralFile)
 			data=JSON.parse(fs.readFileSync(p))
 		} catch(e) {console.log("error:",e)}
 		return data
 	},
 
 	createDrr({projectId, drrId}){
-		console.log("drrCreate resolver...")		
-		try {
-			var p=path.join(site, projectId, branch, drrId)
+		console.log("drrCreate resolver...")
+		var data=new Drr()		
+		try {			
+			var p=path.join(casite, projectId, branch, drrId)
 			//make folder
 			fsp.makeTreeSync(p)
 			//make datafiles in folder
-			fs.writeFileSync(path.join(p, DrrHeadFile), JSON.stringify([DrrHead]))
-			fs.writeFileSync(path.join(p, DrrGeneralFile), JSON.stringify([DrrGeneral]))
-			fs.writeFileSync(path.join(p, DeficiencyFile), JSON.stringify([Deficiency]))
+			fs.writeFileSync(path.join(p, DrrHeadFile), JSON.stringify(data.drrHead))
+			fs.writeFileSync(path.join(p, DrrGeneralFile), JSON.stringify(data.drrGenerals))
+			fs.writeFileSync(path.join(p, DeficiencyFile), JSON.stringify(data.deficiencies))
 			//make rooms folder
 			fsp.makeTreeSync(path.join(p, DeficiencyByRoomFolder))
 
 		} catch(e) {console.log("error:",e)}
 		//return default data
-		return DRR
+		return data
 	}
 }
 
