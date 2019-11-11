980595cbc450f8b732fa375c9dcb2118908188c1
a-siddeley <asiddeley@rogers.com>
2019-11-11 00:55:18 -0500

    electron casbah contextmenu

--- electron/casbah.js
+++ electron/casbah.js
@@ -24,25 +24,50 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 ********************************/
 
-var remote = require('electron').remote;
-var windowManager = remote.require('electron-window-manager');
-//var fs=require('fs');
-var Vue=require('../node_modules/vue/dist/vue.common.js');
-var GoogleSheet	= require('google-spreadsheet');	
-var googleCASBAH = new GoogleSheet("1tKvabqktU80rAFZ2PEC6-iDQwI2DwG3xKLcKLoI16N4");
-var secret = require('../private/client_secret.json');
+var remote = require('electron').remote
+var windowManager = remote.require('electron-window-manager')
+const FSP=require('fs-plus')
+const PATH=require('path')
+var Vue=require('../node_modules/vue/dist/vue.common.js')
+var GoogleSheet	= require('google-spreadsheet')	
+var googleCASBAH = new GoogleSheet("1tKvabqktU80rAFZ2PEC6-iDQwI2DwG3xKLcKLoI16N4")
+var secret = require('../private/client_secret.json')
 var {getOwn, cryptoId, addDays}=require("../server/support.js")
-var projectVue
+
+///// get local settings
+var settings = new Settings()
+
+
+function Settings(){
+	var local={}
+	try { local = require('../private/settings.json')}
+	catch(err){	}	
+	
+	this.currentprojectid=local.currentprojectid||0
+	console.log('settings.currentprojectid:',this.currentprojectid)
+	this.hidden=[]
+	this.stringify=function(data){return JSON.stringify(this)}
+	this.set=function(name, val){
+		if (name && val){this[name]=val}
+		//save to local json
+		var path=PATH.join(__dirname,'../private/settings.json')
+		console.log("Saving settings to: ",path)
+		FSP.writeFileSync(path, this.stringify())
+	}
+}
+
 
 function googleAuth(){
 	googleCASBAH.useServiceAccountAuth(secret, function(){
 		//authenticated so proceed
 		googleCASBAH.getRows(1, function(err, rows){
 			//update vue model...
-			projectVue.rows=rows;
+			projectPicker.rows=rows;
 		})
 	})	
 }	
+
+
 
 function Project({projectno, projectcode, days}){	
 	var today=new Date()
@@ -67,10 +92,38 @@ function Project({projectno, projectcode, days}){
 	this.about="TBD"
 }
 
-///// Custom Directives
-Vue.directive('add-class-hover', {
+Project.prototype.toString=function(data){
+	var project=data||this
+	return Object.getOwnPropertyNames(this).map(function (val, index, array) {
+		return (val + ' -> ' + project[val])
+	}).join('\n')
+}
+
+
+
+///// Extend jQuery
+$.fn.showAtMouse=function(ev){
+	var win$=$(window)
+	var wx=win$['width'](), wy=win$['height'](), sx=win$['scrollLeft'](), sy=win$['scrollTop']()
+	return this.each(function(){
+		var menu$=$(this)
+		var mx=menu$['width'](), my=menu$['height']()
+		menu$.show().css({
+			position:'absolute', 
+			// Horizontally flips menu if too close to bottom
+			left:(ev.pageX + mx > wx && mx < ev.pageX)?ev.pageX+sx-mx:ev.pageX+sx,
+			// Vertically flips menu if too close to right 
+			top:(ev.pageY + my > wy && my < ev.pageY)?ev.pageY+sy-my:ev.pageY+sy
+		})
+		return false
+	})
+}
+
+
+///// Custom Vue Directive
+Vue.directive('class-hover', {
 	bind(el, binding, vnode) {    
-		const { value="" } = binding
+		const { value='' } = binding
 		el.addEventListener('mouseenter',()=> {el.classList.add(value)})
 		el.addEventListener('mouseleave',()=> {el.classList.remove(value)})
 	},
@@ -80,50 +133,47 @@ Vue.directive('add-class-hover', {
 	}
 })
 
+
+///// EXPORTS
 exports.ready=function(){
-	const WIN$=$(window)
-	projectPicker=new Vue({
+
+	var projectPicker=new Vue({
 		el:'#PROJECT-PICKER',
 		data:{
 			title:'click',
 			hoverText:'click to select',
-			rows:[new Project({}), new Project({}), new Project({})],
+			rows:[
+				new Project({projectno:'P-101'}), 
+				new Project({projectno:'P-102'}),
+				new Project({projectno:'P-103'})
+			],
 			isMouseover:false,
-			HIGHLIGHT_CLASS:'highlight'
+			currentprojectid:0
 		},	
 		methods:{
 			isOdd(i){return (i%2===1)},
-			contextMenu:function(ev, pn){
-				console.log("CM...", pn)
-				function menuX(mouse){
-					var win=WIN$['width'](), 
-					menu=CM$['width'](),
-					scroll=WIN$['scrollLeft'](),
-					position = mouse + scroll
-					return (mouse + menu > win && menu < mouse)? position - menu : position
-				}
-				function menuY(mouse){
-					var win=WIN$['height'](), 
-					menu=CM$['height'](),
-					scroll=WIN$['scrollTop'](),
-					position = mouse + scroll
-					return (mouse + menu > win && menu < mouse)? position - menu : position
-				}			
-
-				CM$.show().css({
-					position:'absolute',
-					left:menuX(ev.pageX),
-					top:menuY(ev.pageY)
-				})
-				return false
+			isCurrentProject(id){return this.currentprojectid===id},
+			currentproject(id){
+				//remember to save and retrieve from localstorage during lifecycle
+				this.currentprojectid=id
+				//keep local copy
+				settings.set('currentprojectid', id)
+			},
+			titleText(id){
+				return (id==this.currentprojectid)?
+				'Current project ( '+id+' )':
+				'Click to set as current project'
 			}
-		}		
-	});
-	//googleAuth();
-
-		
-		
+		}
+	})
+	projectPicker.currentprojectid=settings.currentProjectid
+	
+	//console.log("Current Project: ", projectPicker.currentprojectid)
+	return {projectPicker:projectPicker}
 } 
+
+exports.googleAuth=googleAuth
+exports.Project=Project
 
 exports.view=function(a){
 	alert(a)	
