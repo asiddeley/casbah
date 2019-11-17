cefc6eb481041a1fb5179878cdd986d958898f7d
a-siddeley <asiddeley@rogers.com>
2019-11-17 14:27:21 -0500

    electron bootstrapVue caProjects

--- electron/casbah.js
+++ electron/casbah.js
@@ -24,6 +24,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 ********************************/
 
+
 ///// IMPORTS
 const FSP=require('fs-plus')
 const PATH=require('path')
@@ -31,23 +32,18 @@ const PATH=require('path')
 var remote = require('electron').remote
 var windowManager = remote.require('electron-window-manager')
 
-var Vue=require('../node_modules/vue/dist/vue.common.js')
-var BootstrapVue=require('../node_modules/bootstrap-vue/dist/bootstrap-vue.common.js')
-Vue.use(BootstrapVue)
-
 var GoogleSheet	= require('google-spreadsheet')	
-var googleCASBAH = new GoogleSheet("1tKvabqktU80rAFZ2PEC6-iDQwI2DwG3xKLcKLoI16N4")
+var gsProjects = new GoogleSheet("1tKvabqktU80rAFZ2PEC6-iDQwI2DwG3xKLcKLoI16N4")
 var secret = require('../private/client_secret.json')
 
 var {getOwn, cryptoId, addDays, LocalStore}=require("../electron/support.js")
-var casbahVue
 
-function googleAuth(){
-	googleCASBAH.useServiceAccountAuth(secret, function(){
+function googleAuth(vue){
+	gsProjects.useServiceAccountAuth(secret, function(){
 		//authenticated so proceed
-		googleCASBAH.getRows(1, function(err, rows){
-			//update vue model...
-			casbahVue.rows=rows;
+		gsProjects.getRows(1, function(err, rows){
+			rows.forEach(function(r){r._rowVariant=''})
+			casbahVue.rows=rows
 		})
 	})	
 }	
@@ -82,6 +78,12 @@ Project.prototype.toString=function(data){
 		return (val + ' -> ' + project[val])
 	}).join('\n')
 }
+
+Project.prototype.template=function(){
+	return ("<b-table striped hover small :items='rows' :fields='fields' @row-clicked='click' @row-contextmenu='menu'></b-table>")
+}
+
+
 //project instance used for its prototype functions ie. project.toString(data) 
 var project=new Project({})
 
@@ -91,6 +93,7 @@ function menuShow(row, index, e) {
 	casbahVue.menuData=row||{}
 	var wx=window.innerWidth, wy=window.innerHeight, sx=window.scrollX, sy=window.scrollY
 	var menu = document.getElementById("CONTEXT-MENU")
+	menu.style.display = 'block'
 	//var mx=menu.clientWidth, my=menu.clientHeight
 	var mx=menu.scrollWidth, my=menu.scrollHeight
 	//console.log('menu mx, my:', mx, my, menu)
@@ -98,16 +101,27 @@ function menuShow(row, index, e) {
 	menu.style.left=((e.pageX + mx > wx && mx < e.pageX)?e.pageX+sx-mx:e.pageX+sx )+'px'
 	//menu.style.top = e.pageY + 'px'
 	menu.style.top=((e.pageY + my > wy && my < e.pageY)?e.pageY+sy-my:e.pageY+sy)+'px'
-	menu.style.display = 'block'
-		
+
 }
+
+Vue.component('ca-projects',{
+	data:function(){},
+	props:['rows','fields','click','menu'],
+	template:`<b-table striped hover small 
+	:items='rows' 
+	:fields='fields'
+	@row-clicked='click' 
+	@row-contextmenu='menu'
+	></b-table>`,
+})
+
 
 ///// EXPORTS
 exports.ready=function(){
 
 	var settings = new LocalStore(		
 		PATH.join(__dirname,'../private/settings.json'),
-		{currentprojectid:'0', hidden:[]}		
+		{projectid:'0', hidden:[]}		
 	)
 	
 	casbahVue=new Vue({
@@ -124,36 +138,39 @@ exports.ready=function(){
 				{key:'projectno', sortable:true},
 				{key:'project', sortable:true},
 				{key:'subprojectcode', sortable:true},
-				{key:'subproject', dortable:true}
+				{key:'subproject', sortable:true}
 			],
-			currentprojectid:settings.currentprojectid,
-			
+			projectid:settings.projectid,
+			caProjects:true,			
 			menuData:{},
 			//utility
 			project:project
 		},	
 		methods:{
 			alert(msg){alert(msg)},
-			googleAuth(){googleAuth()},			
+			googleAuth(){googleAuth(this)},			
 			makeProjectCurrent(row, index, event){
 				//turn off previous highlight
-				var that=this
-				var oldCurrent=this.rows.find(function(r){return r.projectid==that.currentprojectid})
+				var vue=this
+				var oldCurrent=this.rows.find(function(r){return r.projectid==vue.projectid})
 				if(oldCurrent){oldCurrent._rowVariant=''}
-				//console.log('oldCURRENT:', oldCurrent)
 				
 				//highlight currently clicked row
 				var current=this.rows.find(function(r){return r.projectid==row.projectid})
 				if(current){current._rowVariant='primary'}
-				//console.log('CURRENT:', current)		
 				
 				//update model
-				this.currentprojectid=row.projectid
+				vue.projectid=row.projectid
+				
 				//update local store
-				settings.set('currentprojectid', row.projectid)
+				settings.set('projectid', row.projectid)
+				
+				//update browser table
+				document.getElementsByTagName('title')[0].innerText='CASBAH ' +
+				'('+row.projectno + '/' + row.projectcode + '/' + row.subprojectcode +')'
 			},
 			titleText(id){
-				return (id==this.currentprojectid)?
+				return (id==this.projectid)?
 				'Current project ( '+id+' )':
 				'Click to set as current project'
 			},
@@ -167,15 +184,24 @@ exports.ready=function(){
 	
 	//highlight current project
 	casbahVue.rows.forEach(function(r){
-		if (r.projectid==settings.currentprojectid) {casbahVue.makeProjectCurrent(r)}
+		if (r.projectid==settings.projectid) {casbahVue.makeProjectCurrent(r)}
 	})
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	return {casbahVue:casbahVue}
 } 
 
-exports.googleAuth=googleAuth
-exports.Project=Project
-
-exports.view=function(a){
-	alert(a)	
-}
